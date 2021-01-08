99735569231629ea20fbdb805c8791a21f833686
Kaloyan Marinov <kallo.marinov@gmail.com>
2021-01-08 05:50:16 +0200

    add more examples of using types

--- types.ts
+++ types.ts
@@ -8,8 +8,10 @@ $ node_modules/typescript/bin/tsc types.ts -w
 
 let myString: string;
 let myNum: number;
+let myBool: boolean;
 
-myString = 'Hello World'
-myNum = 'asdasad'
+myString = 'Hello World' // or:  'Hello' + ' ' + 'World' ; 'Hello'.slice(0, 3)
+myNum = 22 // or: 5 ; 5 + 5 ; 5.5 ; 0xf00d
+myBool = true
 
-console.log(myNum);
+console.log(myBool);
