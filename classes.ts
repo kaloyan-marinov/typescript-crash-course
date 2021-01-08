bd90d6b06df83647cee95fecea9367901dbc253c
Kaloyan Marinov <kallo.marinov@gmail.com>
2021-01-08 07:24:16 +0200

    implement a child class in classes.ts

--- classes.ts
+++ classes.ts
@@ -20,10 +20,28 @@ class User {
   // private register() {
   //   console.log(`${this.name} is now registered`);
   // }
+
+  payInvoice() {
+    console.log(`${this.name} is paying an invoice...`)
+  }
+}
+
+class Member extends User {
+  id: number;
+
+  constructor(id: number, name: string, email: string, age: number) {
+    super(name, email, age);
+    this.id = id;
+  }
+
+  payInvoice() {
+    super.payInvoice()
+  }
 }
 
 let john = new User('John Doe', 'john.doe@gmail.com', 34);
-
 console.log(john.age);
-
 john.register();
+
+let mike: User = new Member(1, 'Mike Smith', 'mike.smith@hmail.com', 33)
+mike.payInvoice();
