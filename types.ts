aa6c0e160ff60e0ca2aaf382c122ede7362d36a2
Kaloyan Marinov <kallo.marinov@gmail.com>
2021-01-08 05:46:28 +0200

    show what happens when a value of the wrong type is assigned to a TypeScript variable

--- types.ts
+++ types.ts
@@ -1,10 +1,15 @@
 console.log('Hello from ts');
 /*
 $ node_modules/typescript/bin/tsc types.ts
+
+The following runs the TypeScript compiler in watch mode:
+$ node_modules/typescript/bin/tsc types.ts -w
 */
 
 let myString: string;
+let myNum: number;
 
 myString = 'Hello World'
+myNum = 'asdasad'
 
-console.log(myString);
+console.log(myNum);
