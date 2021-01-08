67141bab05bb1fe5c287ee0e1a07ca1e42046c66
Kaloyan Marinov <kallo.marinov@gmail.com>
2021-01-08 06:00:31 +0200

    wrap up the examples of the simple data types

--- types.ts
+++ types.ts
@@ -6,12 +6,16 @@ The following runs the TypeScript compiler in watch mode:
 $ node_modules/typescript/bin/tsc types.ts -w
 */
 
+/* The simple data types */
 let myString: string;
 let myNum: number;
 let myBool: boolean;
+let myVar: any;
 
 myString = 'Hello World' // or:  'Hello' + ' ' + 'World' ; 'Hello'.slice(0, 3)
 myNum = 22 // or: 5 ; 5 + 5 ; 5.5 ; 0xf00d
 myBool = true
+myVar = '5' // or: 'Hello' ; true
 
-console.log(myBool);
+console.log('The simple data types:');
+console.log(myVar);
